60194c1a7a920f27c16dc7a8f8d1075a79d34d2f
sshworld <59462537+sshworld@users.noreply.github.com>
2024-03-22 08:15:42 +0900

    feat: category controller 구현

---
+++ test_1/board/src/main/java/test/hanteo/board/response/CategoryResponse.java
@@ -0,0 +1,10 @@
+package test.hanteo.board.response;
+
+import test.hanteo.board.infrastructure.Category;
+
+public class CategoryResponse {
+
+    public CategoryResponse(Category category) {
+
+    }
+}
