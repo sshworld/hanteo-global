d3a0e135035d4ddb5890d1a53d696c52c974e9db
sshworld <59462537+sshworld@users.noreply.github.com>
2024-03-22 20:14:12 +0900

    remove: gender category 제거

--- test_1/board/src/main/java/test/hanteo/board/infrastructure/GenderCategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package test.hanteo.board.infrastructure;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GenderCategoryRepository extends JpaRepository<GenderCategory, GenderCategory> {
-    Optional<GenderCategory> findByCategoryId(Long id);
-}
--- test_1/board/src/main/java/test/hanteo/board/request/GenderCreateRequest.java
+++
@@ -1,9 +0,0 @@
-package test.hanteo.board.request;
-
-import lombok.Getter;
-
-@Getter
-public class GenderCreateRequest {
-
-    private String type;
-}
