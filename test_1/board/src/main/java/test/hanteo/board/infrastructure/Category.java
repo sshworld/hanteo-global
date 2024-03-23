1ba814dcf82731ad62b06793be77e753e858011d
sshworld <59462537+sshworld@users.noreply.github.com>
2024-03-23 17:27:04 +0900

    fix: category 도메인 수정

--- test_1/board/src/main/java/test/hanteo/board/infrastructure/Category.java
+++ test_1/board/src/main/java/test/hanteo/board/infrastructure/Category.java
@@ -5,16 +5,18 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import java.time.LocalDateTime;
+import static java.time.LocalDateTime.now;
 import java.util.List;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import test.hanteo.board.request.CategoryCreateRequest;
 
 @Entity(name = "category")
+@Getter
 @NoArgsConstructor
 public class Category {
 
@@ -24,12 +26,11 @@ public class Category {
 
     private String name;
 
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "parent_id")
+    @OneToMany(mappedBy = "parentId", fetch = FetchType.LAZY)
     private List<CategoryRelationship> categoryRelationships;
 
     @CreatedDate
-    private LocalDateTime createdAt;
+    private LocalDateTime createdAt = now();
 
     @LastModifiedDate
     private LocalDateTime updatedAt;
--- test_1/board/src/main/java/test/hanteo/board/infrastructure/CategoryRelationship.java
+++ test_1/board/src/main/java/test/hanteo/board/infrastructure/CategoryRelationship.java
@@ -1,16 +1,27 @@
 package test.hanteo.board.infrastructure;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.io.Serializable;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Getter
+@IdClass(CategoryRelationship.class)
 @NoArgsConstructor
-public class CategoryRelationship {
+public class CategoryRelationship implements Serializable {
 
     @Id
+    @JoinColumn(name = "parent_id")
     private Long parentId;
 
     @Id
-    private Long childId;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "child_id")
+    private Category child;
 }
